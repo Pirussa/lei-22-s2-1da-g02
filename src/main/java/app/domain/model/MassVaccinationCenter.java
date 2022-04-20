37d1164129225e46844bf68d6115458222e2d60f
João Castro <1210816@isep.ipp.pt>
2022-04-20 09:04:23 +0100

    Changing DM and adding US010

--- src/main/java/app/domain/model/MassVaccinationCenter.java
+++ src/main/java/app/domain/model/MassVaccinationCenter.java
@@ -1,5 +1,8 @@
 package app.domain.model;
-
+/**
+ *
+ * @author João Castro <1210816@isep.ipp.pt>
+ */
 public class MassVaccinationCenter extends  VaccinationCenter{
     public MassVaccinationCenter(String name, String address, double phoneNumber, String emailAddress, double faxNumber, String websiteAddress, int openingHour, int closingHour, int slotDuration, int vaccinesPerSlot, String coordinator) {
         super(name, address, phoneNumber, emailAddress, faxNumber, websiteAddress, openingHour, closingHour, slotDuration, vaccinesPerSlot, coordinator);
