87c2b8c2bb2964df80dff85756d7407f488297c0
João Castro <1210816@isep.ipp.pt>
2022-04-27 10:43:57 +0100

    US09 - Updating MD and editing model files

--- src/main/java/app/domain/model/MassVaccinationCenter.java
+++ src/main/java/app/domain/model/MassVaccinationCenter.java
@@ -3,10 +3,13 @@ package app.domain.model;
  *
  * @author João Castro <1210816@isep.ipp.pt>
  */
+
+
 public class MassVaccinationCenter extends  VaccinationCenter{
-    public MassVaccinationCenter(String name, String address, double phoneNumber, String emailAddress, double faxNumber, String websiteAddress, int openingHour, int closingHour, int slotDuration, int vaccinesPerSlot, String coordinator) {
-        super(name, address, phoneNumber, emailAddress, faxNumber, websiteAddress, openingHour, closingHour, slotDuration, vaccinesPerSlot, coordinator);
+
+    private int vaccineCode;
+
+    public MassVaccinationCenter(int coordinatorID, String name, String address, double phoneNumber, String emailAddress, double faxNumber, String websiteAddress, int openingHour, int closingHour, int slotDuration, int vaccinesPerSlot, int vaccineCode) {
+        super(coordinatorID, name, address, phoneNumber, emailAddress, faxNumber, websiteAddress, openingHour, closingHour, slotDuration, vaccinesPerSlot);
     }
-
-
 }
--- src/main/java/app/domain/model/VaccinationCenter.java
+++ src/main/java/app/domain/model/VaccinationCenter.java
@@ -3,7 +3,7 @@ package app.domain.model;
  *
  * @author João Castro <1210816@isep.ipp.pt>
  */
-public class VaccinationCenter {
+public class VaccinationCenter{
 
     private String name;
     private String address;
@@ -15,21 +15,24 @@ public class VaccinationCenter {
     private int closingHour;
     private int slotDuration;
     private int vaccinesPerSlot;
-    private String coordinator;
+    private int coordinatorID;
 
-    private final String NAMEBYOMISSION = "N/A";
-    private final String ADDRESSBYOMISSION = "N/A";
-    private final double PHONENUMBERBYOMISSION = 0;
-    private final String EMAILADDRESSBYOMISSION = "N/A";
-    private final double FAXNUMBERBYOMISSION = 0;
-    private final String WEBSITEADDRESSBYOMISSION = "N/A";
-    private final int OPENINGHOURBYOMISSION = 0;
-    private final int CLOSINGHOURBYOMISSION = 0;
-    private final int SLOTDURATIONBYOMISSION = 0;
-    private final int VACCINESPERSLOTBYOMISSION = 0;
-    private final String COORDINATORBYOMISSION = "N/A";
+   //private final String NAMEBYOMISSION = "N/A";
+   //private final String ADDRESSBYOMISSION = "N/A";
+   //private final double PHONENUMBERBYOMISSION = 0;
+   //private final String EMAILADDRESSBYOMISSION = "N/A";
+   //private final double FAXNUMBERBYOMISSION = 0;
+   //private final String WEBSITEADDRESSBYOMISSION = "N/A";
+   //private final int OPENINGHOURBYOMISSION = 0;
+   //private final int CLOSINGHOURBYOMISSION = 0;
+   //private final int SLOTDURATIONBYOMISSION = 0;
+   //private final int VACCINESPERSLOTBYOMISSION = 0;
+   //private final String COORDINATORBYOMISSION = "N/A";
 
-    public VaccinationCenter(String name, String address, double phoneNumber, String emailAddress, double faxNumber, String websiteAddress, int openingHour, int closingHour, int slotDuration, int vaccinesPerSlot, String coordinator) {
+    Company x;
+
+    public VaccinationCenter(int coordinatorID, String name, String address, double phoneNumber, String emailAddress, double faxNumber, String websiteAddress, int openingHour, int closingHour, int slotDuration, int vaccinesPerSlot) {
+        this.coordinatorID = coordinatorID;
         this.name = name;
         this.address = address;
         this.phoneNumber = phoneNumber;
@@ -40,12 +43,13 @@ public class VaccinationCenter {
         this.closingHour = closingHour;
         this.slotDuration = slotDuration;
         this.vaccinesPerSlot = vaccinesPerSlot;
-        this.coordinator = coordinator;
+
     }
 
     @Override
     public String toString() {
         return "VaccinationCenter{" +
+                "coordinator ID='" + coordinatorID + '\'' +
                 "name='" + name + '\'' +
                 ", address='" + address + '\'' +
                 ", phoneNumber=" + phoneNumber +
@@ -56,14 +60,10 @@ public class VaccinationCenter {
                 ", closingHour=" + closingHour +
                 ", slotDuration=" + slotDuration +
                 ", vaccinesPerSlot=" + vaccinesPerSlot +
-                ", coordinator='" + coordinator + '\'' +
                 '}';
     }
 
     public boolean validateVaccinationCenter(){
-        if (name.isEmpty()||address.isEmpty()||emailAddress.isEmpty()||websiteAddress.isEmpty()||coordinator.isEmpty()){
-            return  false;
-        }
-        return true;
+       return false;
     }
 }
