4ba9a76af8a65af627fef24d5f30a13e8a00df9f
GuilhermeSousa03 <1211073@isep.ipp.pt>
2022-05-05 16:49:08 +0100

    US010 - Implementation almost completed. All validations are working just fine except for the phone number one.

--- src/main/java/app/ui/console/RegisterNewEmployeeUI.java
+++ src/main/java/app/ui/console/RegisterNewEmployeeUI.java
@@ -26,41 +26,43 @@ public class RegisterNewEmployeeUI implements Runnable {
 
     public void run() {
 
+        Scanner read = new Scanner(System.in);
         RegisterNewEmployeeController ctrl = new RegisterNewEmployeeController();
-
+        int option;
         RegisterNewEmployeeDto dto = new RegisterNewEmployeeDto();
-        System.out.printf("%n--------------------------------------%nYou are now registering a new employee%n%n");
+        System.out.printf("%n--Employee Registration--%n%n");
 
         ArrayList<String> roles = new ArrayList<>();
         roles.add("Nurse");
         roles.add("Receptionist");
         roles.add("Center Coordinator");
 
-        int index = Utils.showAndSelectIndex(roles, "Select a Role: ");
-        String selectedRole = roles.get(index);
+        //do {
+            int index = Utils.showAndSelectIndex(roles, "Select a Role: ");
+            String selectedRole = roles.get(index);
 
-        dto.id = String.valueOf(Employee.idGenerator(selectedRole));
-        dto.password = Employee.passwordGenerator();
+            dto.id = String.valueOf(Employee.idGenerator(selectedRole));
+            dto.password = Employee.passwordGenerator();
 
-        dto.name = Utils.readLineFromConsole("Insert Name: ");
-        dto.address = Utils.readLineFromConsole("Insert Address: ");
-        dto.phoneNumber = (Utils.readLineFromConsole("Insert Phone Number: "));
-        dto.email = Utils.readLineFromConsole("Insert Email: ");
-        dto.citizenCardNumber = (Utils.readLineFromConsole("Insert Citizen Card Number: "));
+            dto.name = Utils.readLineFromConsole("Insert Name: ");
+            dto.address = Utils.readLineFromConsole("Insert Address (Street, Zip Code, location): ");
+            dto.phoneNumber = Utils.readLineFromConsole("Insert Phone Number (9 chars, only numbers): ");
+            dto.email = Utils.readLineFromConsole("Insert Email (@ and . are required): ");
+            dto.citizenCardNumber = Utils.readLineFromConsole("Insert Citizen Card Number (12 chars, 10 numbers and 2 letters): ");
 
-        if (ctrl.registerNewEmployee(dto)) {
-            showNewEmployeeData(dto, selectedRole);
-            if (Utils.confirmCreation()) {
-                ctrl.saveCreatedEmployee(dto, selectedRole);
-                System.out.printf("%n%n**The Employee was registered.**");
-            } else
-                System.out.println("**The Employee was not registered.**");
-        } else {
-            System.out.println("Invalid Data for the Employee, please fix the issue if you want to register an Employee.");
-            showNewEmployeeData(dto, selectedRole);
-        }
+            if (ctrl.registerNewEmployee(dto)) {
+                showNewEmployeeData(dto, selectedRole);
+                if (Utils.confirmCreation()) {
+                    ctrl.saveCreatedEmployee(dto, selectedRole);
+                    System.out.printf("%n%nThe Employee was registered.");
+                } else
+                    System.out.println("The Employee was not registered.");
+            } else {
+                System.out.println("Invalid Data for the Employee, please fix the issue if you want to register an Employee.");
+                showNewEmployeeData(dto, selectedRole);
+            }
+       // } while (!ctrl.registerNewEmployee(dto));
     }
-
     public void showNewEmployeeData(RegisterNewEmployeeDto dto, String selectedRole) {
         System.out.printf("**New Employee Data**%n%nSelected Role: %s%n%nGiven Name: %s%n%nGiven Address: %s%n%nGiven Phone Number: %s%n%nGiven Email: %s%n%nGiven Citizen Card Number: %s%n%nGenerated ID: %s%n%nGenerated Password: %s%n%n", selectedRole, dto.name, dto.address, dto.phoneNumber, dto.email, dto.citizenCardNumber, dto.id, dto.password);
     }
--- src/main/java/app/ui/console/utils/Utils.java
+++ src/main/java/app/ui/console/utils/Utils.java
@@ -197,5 +197,4 @@ public class Utils {
         c.saveVaccine(dto);
 
     }
-
 }
