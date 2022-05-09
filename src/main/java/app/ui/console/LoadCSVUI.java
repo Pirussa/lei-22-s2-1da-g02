e9a4e2a9d4b35b13eb0f5061951291b000537e49
João Castro <1210816@isep.ipp.pt>
2022-05-09 18:20:07 +0100

    US14 - Can now load and save a CSV file.

--- src/main/java/app/ui/console/LoadCSVUI.java
+++ src/main/java/app/ui/console/LoadCSVUI.java
@@ -10,6 +10,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LoadCSVUI implements Runnable {
@@ -17,20 +18,26 @@ public class LoadCSVUI implements Runnable {
     public void run() {
 
         System.out.println("");
-        System.out.println("Do you want to load a CSV file?");
         if (Utils.confirmCreation()) {
             try{
                 Scanner readPath = new Scanner(System.in);
                 System.out.println("File Location: ");
+                System.out.println();
                 String path = readPath.nextLine();
+                ArrayList<String> csvData = new ArrayList<>();
                 if (validateFileFormat(path)){
                     String line ="";
                     BufferedReader br = new BufferedReader(new FileReader(path));
-
+                    br.readLine();
                     while((line = br.readLine()) != null){
                         String[] values = line.split(",");
-                        System.out.println("EX1: " + values[0] + ", EX2: " + values[1]);
+                        if (validateCSVData(line, values)){
+                            csvData.add("\nName: "+values[0] + " SNS User Number: " + values[1]);
+                        }else{
+                            throw new IllegalArgumentException("The CSV data is invalid, .i.e, the Name of the User has non-word character.");
+                        }
                     }
+                    System.out.println(csvData);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -44,15 +51,11 @@ public class LoadCSVUI implements Runnable {
         return path.endsWith(".csv");
     }
 
-}
+    public boolean validateCSVData(String line, String[] values) throws IOException {
+            values = line.split(",");
+        return !values[0].isEmpty() && !values[1].isEmpty() && values[1].trim().matches("^[0-9]*$");
+        }
+    }
 
 
-//Scanner readPath = new Scanner(System.in);
-//System.out.println("File Location: ");
-//String path = readPath.nextLine();
-//if (validateFileFormat(path)){
-//    System.out.println("File Format is valid.");
-//}
-//else{
-//    throw new IllegalArgumentException("No file found or it's not a .csv file.");
-//}
+
