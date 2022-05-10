74e91fabb8c3a810cedc249eec69a6280866e344
GuilhermeSousa03 <1211073@isep.ipp.pt>
2022-05-10 12:43:37 +0100

    US002 - ScheduleVaccinationController and ScheduleVaccinationUI. Started implementing both classes.

---
+++ src/main/java/app/controller/ScheduleVaccinationController.java
@@ -0,0 +1,17 @@
+package app.controller;
+
+import app.domain.model.Company;
+
+/**
+ * US010 - Register New Employee Controller
+ *
+ * @author Guilherme Sousa <1211073@isep.ipp.pt>
+ */
+
+
+public class ScheduleVaccinationController {
+
+    private Company company = App.getInstance().getCompany();
+
+    public ScheduleVaccinationController() {}
+}
--- src/main/java/app/ui/console/ReceptionistUI.java
+++ src/main/java/app/ui/console/ReceptionistUI.java
@@ -1,9 +1,33 @@
 package app.ui.console;
 
-public class ReceptionistUI implements Runnable{
+import app.ui.console.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReceptionistUI implements Runnable {
 
     @Override
     public void run() {
+        List<MenuItem> options = new ArrayList<>();
 
+        options.add(new MenuItem("Schedule a vaccination.", new CreateVaccinationCenterUI()));
+        options.add(new MenuItem("Register the arrival of a SNS user to take the vaccine.", new MainMenuUI()));
+
+        options.add(new MenuItem("Logout.",new MainMenuUI() ));
+
+
+
+
+
+        int option = 0;
+        do {
+            option = Utils.showAndSelectIndex(options, "\n\nReceptionist Menu:");
+
+            if ((option >= 0) && (option < options.size())) {
+                options.get(option).run();
+            }
+        }
+        while (option != -1);
     }
 }
