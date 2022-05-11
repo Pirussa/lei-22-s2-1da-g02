9abb93a6e2bcacdf894e7483cd00c0ac608098ab
GuilhermeSousa03 <1211073@isep.ipp.pt>
2022-05-11 10:48:54 +0100

    US002 - US002_ScheduleVaccination.md: Customer Specifications and clarifications, Acceptance Criteria, Found out Dependencies, Input and Output Data and System Sequence Diagram.

--- src/main/java/app/ui/console/LoadCSVUI.java
+++ src/main/java/app/ui/console/LoadCSVUI.java
@@ -16,44 +16,44 @@ public class LoadCSVUI implements Runnable {
     public void run() {
 
         System.out.println("");
-            try{
-                Scanner readPath = new Scanner(System.in);
-                System.out.println("File Location: ");
-                System.out.println();
-                String path = readPath.nextLine();
-                ArrayList<String> csvData = new ArrayList<>();
-                if (validateFileFormat(path)){
-                    String line =null;
-                    BufferedReader br = new BufferedReader(new FileReader(path));
-                    String delimiter=null;
-                    br.mark(100);
-                    if (br.readLine().contains(";")){
-                        delimiter=";";
-                    } else {
-                        delimiter=",";
-                        br.reset();
-                    }
-
-                    while((line = br.readLine()) != null){
-                        String password = Utils.passwordGenerator();
-                        line=line.replaceAll("\"","");
-                        String[] values = line.split(delimiter);
-                        if (validateCSVData(values)){
-                            csvData.add(values[0]+"/"+values[1]+"/"+values[2]+"/"+ password);
-                        }else{
-                            throw new IllegalArgumentException("The CSV data is invalid, .i.e, the Name of the User has non-word character.");
-                        }
-                    }
-                    fillSNSUserDto(csvData);
-                    getListOfSNSUsers();
-                } else{
-                    System.out.println();
-                    System.out.println("Only files ending in .csv are allowed.");
+        try {
+            Scanner readPath = new Scanner(System.in);
+            System.out.println("File Location: ");
+            System.out.println();
+            String path = readPath.nextLine();
+            ArrayList<String> csvData = new ArrayList<>();
+            if (validateFileFormat(path)) {
+                String line = null;
+                BufferedReader br = new BufferedReader(new FileReader(path));
+                String delimiter = null;
+                br.mark(100);
+                if (br.readLine().contains(";")) {
+                    delimiter = ";";
+                } else {
+                    delimiter = ",";
+                    br.reset();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+
+                while ((line = br.readLine()) != null) {
+                    String password = Utils.passwordGenerator();
+                    line = line.replaceAll("\"", "");
+                    String[] values = line.split(delimiter);
+                    if (validateCSVData(values)) {
+                        csvData.add(values[0] + "/" + values[1] + "/" + values[2] + "/" + password);
+                    } else {
+                        throw new IllegalArgumentException("The CSV data is invalid, .i.e, the Name of the User has non-word character.");
+                    }
+                }
+                fillSNSUserDto(csvData);
+                getListOfSNSUsers();
+            } else {
+                System.out.println();
+                System.out.println("Only files ending in .csv are allowed.");
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 
     public boolean validateFileFormat(String path) {
         return path.endsWith(".csv");
@@ -62,24 +62,24 @@ public class LoadCSVUI implements Runnable {
     public boolean validateCSVData(String[] values) throws IOException {
 
         int MAXNUMBEROFCHARSSNSUSERNUMBER = 9;
-        return !values[0].isEmpty() && !values[1].isEmpty() && values[1].trim().matches("^[0-9]*$") && values[1].length()== MAXNUMBEROFCHARSSNSUSERNUMBER
+        return !values[0].isEmpty() && !values[1].isEmpty() && values[1].trim().matches("^[0-9]*$") && values[1].length() == MAXNUMBEROFCHARSSNSUSERNUMBER
                 && !values[2].isEmpty() && Utils.validateEmail(values[2]);
     }
 
-    public void fillSNSUserDto(ArrayList<String> csvData){
+    public void fillSNSUserDto(ArrayList<String> csvData) {
         LoadCSVController controller = new LoadCSVController();
         String[] values;
         for (int i = 0; i < csvData.size(); i++) {
             SNSUserDto dto = new SNSUserDto();
-            values=csvData.get(i).split("/");
-            dto.strName= values[0];
+            values = csvData.get(i).split("/");
+            dto.strName = values[0];
             dto.strSNSUserNumber = values[1];
             dto.strEmail = values[2];
             dto.strPassword = values[3];
             System.out.println();
             System.out.println(controller.createSNSUser(dto));
             System.out.println();
-            if (Utils.confirmCreation()){
+            if (Utils.confirmCreation()) {
                 controller.saveSNSUser(dto);
             } else System.out.println("Not confirmed");
         }
@@ -97,7 +97,4 @@ public class LoadCSVUI implements Runnable {
             System.out.println("There aren't any registered SNS Users.");
         }
     }
-    }
-
-
-
+}
