9d7b2f6735f0afcd3b4e6305e101ffc6f2239c68
GustavoJ6 <1211061@isep.ipp.pt>
2022-05-18 09:59:39 +0100

    commit test

--- src/main/java/app/ui/console/ScheduleVaccineUI.java
+++ src/main/java/app/ui/console/ScheduleVaccineUI.java
@@ -109,7 +109,6 @@ public class ScheduleVaccineUI implements Runnable {
         return SNSNumber;
     }
 
-
     private VaccineType selectVaccineTypeHealthCareCenterUI(HealthcareCenter healthcareCenter) {
         return healthcareCenter.getVaccineTypes().get(Utils.selectFromList(healthcareCenter.getVaccineTypes(), "Select one Vaccine Type"));
     }
