d9e146ffb03429726261fa981915f18cf537602f
João Castro <1210816@isep.ipp.pt>
2022-05-25 09:01:54 +0100

    US14 - Test file added

---
+++ src/test/java/app/domain/model/SnsUserTest.java
@@ -0,0 +1,45 @@
+package app.domain.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SnsUserTest {
+
+    @Test
+    void validateEmail() {
+    }
+
+    @Test
+    void validatePassword() {
+    }
+
+    @Test
+    void validateSNSUserNumber() {
+    }
+
+    @Test
+    void validatePhoneNumber() {
+    }
+
+    @Test
+    void validateCitizenCardNumber() {
+    }
+
+
+    @Test
+    void validateAddress() {
+    }
+
+    @Test
+    void validateSex() {
+    }
+
+    @Test
+    void validateBirthDate() {
+    }
+
+    @Test
+    void validateSNSUser() {
+    }
+}
