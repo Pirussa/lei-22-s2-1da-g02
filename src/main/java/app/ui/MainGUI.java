f0c1d9ed087bb0315678439a02e3a77f0f347927
GustavoJ6 <1211061@isep.ipp.pt>
2022-06-02 21:33:54 +0100

    GENERAL - GUI - Fixing Issues -> GUI was not running -> Recoding MainGUI class; Setting project settings to enable the GUI to run properly

--- src/main/java/app/ui/MainGUI.java
+++ src/main/java/app/ui/MainGUI.java
@@ -1,27 +1,32 @@
 package app.ui;
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class MainGUI extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/app/ui/MainMenu.fxml")));
+
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main-menu.fxml"));
+
+        Scene scene = new Scene(root);
         primaryStage.setTitle("Main Menu");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(scene);
         primaryStage.show();
 
 
     }
+    public static void main(String[] args) {
+        launch();
+    }
+
+
 }
