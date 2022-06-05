49a8fe19bd61f4eee31a3bfd6d10c11e317e4020
GustavoJ6 <1211061@isep.ipp.pt>
2022-06-05 05:13:22 +0100

    US15 - Implementation - Added Controller to the GUI that shows the list with the Vaccination stats - Added methods on both controllers in order to either show the list or to print the list to file. Added askFileNameTxt TextArea so the user can introduce the file name and added okBtn so the user confirms that the name is complete - Everything related to the US implementation is completed, the printing to file just needs a small fix.

--- src/main/java/app/controller/CheckAndExportVaccinationStatsController.java
+++ src/main/java/app/controller/CheckAndExportVaccinationStatsController.java
@@ -1,32 +1,99 @@
 package app.controller;
 
+import app.domain.model.*;
+import pt.isep.lei.esoft.auth.domain.model.Email;
+
 import java.io.File;
 import java.io.PrintWriter;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The type Check and export vaccination stats controller.
+ */
 public class CheckAndExportVaccinationStatsController {
 
+    /**
+     * Instantiates a new Check and export vaccination stats controller.
+     */
     public CheckAndExportVaccinationStatsController() {
+        setCenter();
     }
 
-    public List<String> getVaccinationStats() {
-        return null;
+    private final Company company = App.getInstance().getCompany();
+
+    private VaccinationCenter center;
+
+    private void setCenter(){
+        String id = getLoggedCoordinatorId();
+        center = getVaccinationCenterAssociatedToCoordinator(id);
     }
 
-    //Duas abordagens diferentes possíveis de seguir:
-    //dar return a void e lançar exceção caso dê errado para depois na UI ser apenas printar que foi sucedido (se for) e tratar a exceção
-    //retornar um boolean indicando se o ficheiro foi ou não exportado.
-    public boolean exportVaccinationStats() {
-        File file = new File("PorDefinir.csv");
+    /**
+     * Gets a list with the total of fully vaccinated people per day (each day has a total) .
+     *
+     * @return the vaccination stats
+     */
+    private List<String> getVaccinationStatsList() {
+        assert center != null;
+        List<String> vaccinationStats = new ArrayList<>();
+        List<VaccineBulletin> listFullyVaccinated= center.getFullyVaccinatedList();
+        LocalDate lastDay = getFirstDateAvailable(listFullyVaccinated);
+        int total = 0;
+        for (VaccineBulletin vaccineBulletin: listFullyVaccinated) {
+            if (!vaccineBulletin.getDateTimeOfLastDose().toLocalDate().equals(lastDay)) {
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append(lastDay).append(",").append(total);
+                String statsOfOneDay = stringBuilder.toString();
+                vaccinationStats.add(statsOfOneDay);
+                lastDay = vaccineBulletin.getDateTimeOfLastDose().toLocalDate();
+            }
+            total++;
+        }
+
+        return vaccinationStats;
+    }
+
+    public List<String>getVaccinationStatsListBetweenDates(LocalDate firstDate, LocalDate lastDate){
+        List<String> dailyStats = getVaccinationStatsList();
+        List<String> statsBetweenDates = new ArrayList<>();
+
+        for (String dailyStat: dailyStats) {
+            String[] dailyStatArray = dailyStat.split(",");
+            LocalDate date = LocalDate.parse(dailyStatArray[0]);
+            if (date.isAfter(firstDate) && date.isBefore(lastDate)) {
+                statsBetweenDates.add(dailyStat);
+            }
+        }
+        return statsBetweenDates;
+
+    }
+
+    private LocalDate getFirstDateAvailable(List<VaccineBulletin> listFullyVaccinated) {
+        return listFullyVaccinated.get(0).getDateTimeOfLastDose().toLocalDate();
+    }
+
+    /**
+     * Export vaccination stats boolean.
+     *
+     * @return true if the export was done successfully
+     */
+
+    public boolean exportVaccinationStats(String fileName,LocalDate firstDate, LocalDate lastDate) {
+        fileName = fileName + ".csv";
+        File file = new File(fileName);
         PrintWriter writer = null;
+
         try {
             writer = new PrintWriter(file);
-            writer.println("x");
-
-            //Printar tudo para aqui
+            writer.format("%s,%s\n", "Date", "Total");
 
 
+            for (String stat :getVaccinationStatsListBetweenDates(firstDate,lastDate) ) {
+                writer.format("%s\n", stat);
+            }
+            writer.close();
         } catch (Exception FileNotFoundException) {
             return false;
         } finally {
@@ -37,6 +104,13 @@ public class CheckAndExportVaccinationStatsController {
         return true;
     }
 
+    /**
+     * Check if dates are valid.
+     *
+     * @param firstDate the first date
+     * @param lastDate  the last date
+     * @return an int related to the outcome
+     */
     public int checkIfDatesAreValid(LocalDate firstDate, LocalDate lastDate) {
         int errorCode = 0;
         if (firstDate == null || lastDate == null) {
@@ -57,6 +131,18 @@ public class CheckAndExportVaccinationStatsController {
         return errorCode;
     }
 
+    private VaccinationCenter getVaccinationCenterAssociatedToCoordinator(String coordinatorId) {
+        if (company.getVaccinationCenterAssociatedToCoordinator(coordinatorId) != null ) {
+            return company.getVaccinationCenterAssociatedToCoordinator(coordinatorId);
+        }
+        return null;
+    }
+
+    private String getLoggedCoordinatorId() {
+        Email email = company.getAuthFacade().getCurrentUserSession().getUserId();
+
+        return company.getCoordinatorId(email);
+    }
 }
 
 
