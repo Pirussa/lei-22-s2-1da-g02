2e353846bab56284a61f40999ee2f8a862586689
GuilhermeSousa03 <1211073@isep.ipp.pt>
2022-06-06 18:35:47 +0100

    US008 - |JavaDoc| - Added JavaDoc for the UI and "VaccineBulletinDto".

--- src/main/java/dto/VaccineBulletinDto.java
+++ src/main/java/dto/VaccineBulletinDto.java
@@ -13,6 +13,14 @@ public class VaccineBulletinDto {
 
     public String lotNumber;
 
+    /**
+     * Creates a vaccine bulletin with the following attributes:
+     *
+     * @param vaccine   the vaccine administered
+     * @param dateTime  the date time of the administration
+     * @param doses     the dose number of the administration
+     * @param lotNumber the lot number of the adminstered vaccine
+     */
     public VaccineBulletinDto(Vaccine vaccine, LocalDateTime dateTime, int doses, String lotNumber) {
         this.vaccine = vaccine;
         this.dateTimeOfLastDose = dateTime;
