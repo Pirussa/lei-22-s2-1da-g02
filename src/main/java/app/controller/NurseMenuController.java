ee9f3c32d2cb9cd08da111e5007b71c10f3871de
GuilhermeSousa03 <1211073@isep.ipp.pt>
2022-06-07 16:43:14 +0100

    US008 - |Java FX| - Nurse is now able to move forward and start "Record Vaccine Administration".

---
+++ src/main/java/app/controller/NurseMenuController.java
@@ -0,0 +1,30 @@
+package app.controller;
+
+import app.domain.model.Company;
+
+/**
+ * The Nurse menu controller.
+ *
+ * @author Guilherme Sousa
+ */
+
+public class NurseMenuController {
+
+    private final Company company = App.getInstance().getCompany();
+
+    /**
+     * Checks if the company has enough info for vaccine administration.
+     *
+     * @return the int related to the error or success of the operation
+     */
+    public int vaccineAdministrationRequirments() {
+        if (company.getVaccinationCenters().isEmpty())
+            return 1;
+        if (company.getVaccinesList().isEmpty())
+            return 2;
+        if (company.getVaccineTypes().isEmpty())
+            return 3;
+        return 0;
+    }
+
+}
