faaf5e29a9ecbf075d1a97e261592b9d2e716f28
João Castro <1210816@isep.ipp.pt>
2022-06-13 14:04:17 +0100

    US17 - Tests - Creating file

---
+++ src/test/java/app/miscellaneous/ReadLegacyDataFileTest.java
@@ -0,0 +1,67 @@
+package app.miscellaneous;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * The type Read legacy data file test.
+ */
+class ReadLegacyDataFileTest {
+
+    /**
+     * Read file.
+     */
+    @Test
+    void readFile() {
+    }
+
+    /**
+     * Update legacy file.
+     */
+    @Test
+    void updateLegacyFile() {
+    }
+
+    /**
+     * Merge sort ascending.
+     */
+    @Test
+    void mergeSortAscending() {
+    }
+
+    /**
+     * Merge sort descending.
+     */
+    @Test
+    void mergeSortDescending() {
+    }
+
+    /**
+     * Sets list.
+     */
+    @Test
+    void setList() {
+    }
+
+    /**
+     * Write array to file.
+     */
+    @Test
+    void writeArrayToFile() {
+    }
+
+    /**
+     * Heap sort ascending.
+     */
+    @Test
+    void heapSortAscending() {
+    }
+
+    /**
+     * Heap sort descending.
+     */
+    @Test
+    void heapSortDescending() {
+    }
+}
