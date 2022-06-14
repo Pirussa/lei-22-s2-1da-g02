5dfb0811b5f5560b7e86847fea0622aec87be895
João Castro <1210816@isep.ipp.pt>
2022-06-14 18:36:09 +0100

    US17 - Implementation - Prints now only in the UI.

--- src/main/java/app/ui/console/DataFromLegacySystemUI.java
+++ src/main/java/app/ui/console/DataFromLegacySystemUI.java
@@ -14,11 +14,13 @@ import java.util.*;
 
 public class DataFromLegacySystemUI implements Runnable {
     private final DataFromLegacySystemController controller = new DataFromLegacySystemController();
-    private List<String> sortedList;
+    private List<String> sortedListUI;
 
     public void run() {
         try {
             Scanner readPath = new Scanner(System.in);
+            Scanner scPos = new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in);
             System.out.println();
             System.out.println("File Location: ");
             System.out.println();
@@ -26,8 +28,20 @@ public class DataFromLegacySystemUI implements Runnable {
             controller.readFile(path);
             if (!controller.updateFile().isEmpty()) {
                 System.out.println("File updated successfully");
-                chooseCriteriaToSort();
-                sortListWithAlgo();
+                System.out.println();
+                System.out.println("Choose the option you want to sort.");
+                System.out.println("0 - Arrival Date Time");
+                System.out.println("1 - Leaving Date Time");
+                System.out.println("2 - Back to Menu");
+                int optionPosition = scPos.nextInt();
+                controller.chooseCriteriaToSort(optionPosition);
+                String algorithmToBeUsed = App.getInstance().getSortingAlgorithm();
+                System.out.printf("%nAlgorithm in config file: " + algorithmToBeUsed);
+                System.out.printf("%nChoose the way you want to sort.%n0 - Ascending%n1 - Descending%n2 - Back to Menu%n");
+                int option = scanner.nextInt();
+                sortedListUI=controller.sortListWithAlgo(algorithmToBeUsed,option);
+                System.out.println();
+                printSortedArray(sortedListUI);
             } else {
                 System.out.println("File update failed, probable cause: No User Number/Vaccine is registered in the system.");
             }
@@ -36,73 +50,6 @@ public class DataFromLegacySystemUI implements Runnable {
         }
     }
 
-    public void chooseCriteriaToSort() {
-        Scanner scPos = new Scanner(System.in);
-        final int ArrivalOption = 6;
-        final int LeaveOption = 8;
-        System.out.println();
-        System.out.println("Choose the option you want to sort.");
-        System.out.println("0 - Arrival Date Time");
-        System.out.println("1 - Leaving Date Time");
-        System.out.println("2 - Back to Menu");
-        int optionPosition = scPos.nextInt();
-        switch (optionPosition) {
-            case 0:
-                controller.setList(ArrivalOption);
-                break;
-            case 1:
-                controller.setList(LeaveOption);
-                break;
-            case 2:
-        }
-    }
-
-    public void sortListWithAlgo() {
-        String algorithmToBeUsed = App.getInstance().getSortingAlgorithm();
-
-        System.out.printf("%nAlgorithm in config file: " + algorithmToBeUsed);
-        switch (algorithmToBeUsed) {
-            case "HeapSort":
-                Scanner scanner = new Scanner(System.in);
-                System.out.printf("%nChoose the way you want to sort.%n0 - Ascending%n1 - Descending%n2 - Back to Menu%n");
-                int optionOne = scanner.nextInt();
-                switch (optionOne) {
-                    case 0:
-                         sortedList = controller.heapAscending();
-
-                        break;
-                    case 1:
-                        sortedList = controller.heapDescending();
-
-                        break;
-                    case 2:
-                        break;
-                }
-                break;
-            case "MergeSort":
-                Scanner scTwo = new Scanner(System.in);
-                System.out.println();
-                System.out.println("Choose the way you want to sort.");
-                System.out.println("0 - Ascending");
-                System.out.println("1 - Descending");
-                System.out.println("2 - Back to Menu");
-                int optionTwo = scTwo.nextInt();
-                switch (optionTwo) {
-                    case 0:
-                       sortedList = controller.mergeAscending(controller.getListToSort(), 0, controller.getListToSort().size() - 1);
-                       printSortedArray(sortedList);
-                        break;
-                    case 1:
-                        sortedList =  controller.mergeDescending(controller.getListToSort(), 0, controller.getListToSort().size() - 1);
-                        printSortedArray(sortedList);
-                        break;
-                    case 2:
-                        break;
-                }
-                break;
-        }
-
-    }
 
     public void printSortedArray(List<String> sortedList) {
         System.out.println("Sorted Array:");
