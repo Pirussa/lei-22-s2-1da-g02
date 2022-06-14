19d018564cdcccdaca13fb1cd8786660705db308
joaooleitaoo <1211063@isep.ipp.pt>
2022-06-14 18:14:48 +0100

    US011 - Tests - Small fix regarding some problems with the US011 tests.

--- src/test/java/app/domain/model/ConsultUsersInTheWaitingRoomTest.java
+++ src/test/java/app/domain/model/ConsultUsersInTheWaitingRoomTest.java
@@ -25,14 +25,20 @@ class ConsultUsersInTheWaitingRoomTest {
 
 
     private void setup() {
+
+        VaccineType vt1 = new VaccineType("COVID", "A vaccine to prevent serious infections of the Covid-19 Virus", VaccineType.vaccineTechnologies[5]);
+
         VaccinationCenter vc = new VaccinationCenter("1234", "CVC Matosinhos", "915607321", "cvcmatosinhos@gmail.com", "915607321", "www.cvcmatosinhos.com", "1", "9", "30", "16", "Rua do Amial", "4460-098", "Matosinhos", "CC-95634");
         company.getVaccinationCentersStore().getVaccinationCenters().add(vc);
         ctrl.setVaccinationCenter(0);
 
-        VaccineType vt1 = new VaccineType("COVID", "A vaccine to prevent serious infections of the Covid-19 Virus", VaccineType.vaccineTechnologies[5]);
+        SnsUser snsuser1 = new SnsUser("User Default", "Male", "01/01/1998", "Default # 4000-000 # Default", "915604428", "u@gmail.com", 100000000, "14698413 7 ZY7", "AAA00aa");
+        SnsUser snsuser2 = new SnsUser("User Default1", "Male", "01/01/2003", "Default # 4000-001 # Default", "915604429", "u1@gmail.com", 200000000, "16068893 0 ZX7", "AAA11aa");
+        company.getSnsUsersStore().getSnsUserList().add(snsuser1);
+        company.getSnsUsersStore().getSnsUserList().add(snsuser2);
 
-        ScheduledVaccine appointment1 = new ScheduledVaccine(100000000, vt1, LocalDateTime.of(2022, 5, 25, 10, 0));
-        ScheduledVaccine appointment2 = new ScheduledVaccine(200000000, vt1, LocalDateTime.of(2022, 5, 25, 10, 10));
+        ScheduledVaccine appointment1 = new ScheduledVaccine(100000000, vt1, LocalDateTime.of(2022, 6, 14, 18, 0));
+        ScheduledVaccine appointment2 = new ScheduledVaccine(200000000, vt1, LocalDateTime.of(2022, 6, 14, 18, 10));
 
         vc.getScheduledVaccineList().add(appointment1);
         vc.getScheduledVaccineList().add(appointment2);
@@ -49,15 +55,6 @@ class ConsultUsersInTheWaitingRoomTest {
 
         vc.getArrivalsList().add(arrival1);
         vc.getArrivalsList().add(arrival2);
-
-
-        SnsUser snsuser1 = new SnsUser("User Default", "Male", "01/01/1998", "Default # 4000-000 # Default", "915604428", "u@gmail.com", 100000000, "14698413 7 ZY7", "AAA00aa");
-        SnsUser snsuser2 = new SnsUser("User Default1", "Male", "01/01/2003", "Default # 4000-001 # Default", "915604429", "u1@gmail.com", 200000000, "16068893 0 ZX7", "AAA11aa");
-
-        company.getSnsUsersStore().getSnsUserList().add(snsuser1);
-        company.getSnsUsersStore().getSnsUserList().add(snsuser2);
-
-
     }
 
     /**
@@ -82,7 +79,7 @@ class ConsultUsersInTheWaitingRoomTest {
                         "SNS User Number: " + "200000000" + '\n' +
                         "Phone Number: " + "915604429" + '\n');
 
-        assertEquals(checkList, ctrl.listOfUsersInTheWaitingRoom());
+        assertEquals(checkList,ctrl.listOfUsersInTheWaitingRoom());
     }
 
     /**
