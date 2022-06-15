b61936b8ef04cd465c54a64f6bbfe47018b19587
GustavoJ6 <1211061@isep.ipp.pt>
2022-06-15 17:01:06 +0100

    Merge

--- src/main/java/app/miscellaneous/ReadLegacyDataFile.java
+++ src/main/java/app/miscellaneous/ReadLegacyDataFile.java
@@ -73,8 +73,8 @@ public class ReadLegacyDataFile {
                 if (company.getSnsUsersStore().containsUserWithNumber(Integer.parseInt(values[0]))) {
                     if (company.containsVaccine(values[1])) {
                         updatedList.add(company.getSnsUsersStore().getSnsUserList().get(positionInSnsUserList).getStrName() + "|" + line + "|" + company.getVaccinesList().get(positionInVaccinesList).getVaccineType().getDescription());
-//               setArrival(values[4], values[0]);
-//                        setDeparture(values[7], values[0]);
+                        setArrival(values[4], values[0]);
+                        setDeparture(values[7], values[0]);
                     }
                 }
             }
